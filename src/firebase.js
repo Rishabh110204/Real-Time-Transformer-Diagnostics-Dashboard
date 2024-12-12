408ecca0e93e2209659652447bc625582aeb6cc4
Rishabh110204 <106269785+Rishabh110204@users.noreply.github.com>
2024-12-12 18:28:37 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,14 +7,14 @@ import { getDatabase } from "firebase/database";
 
 
 const firebaseConfig = {
-  databaseURL: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  authDomain: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  projectId: "XXXXXXXXXXX",
-  storageBucket: "XXXXXXXXXXXXXX",
-  messagingSenderId: "XXXXXXXXXXX",
-  appId: "1:XXXXXXXXXXXXXXXXXXXXX",
-  measurementId: "XXXXXXXXXXXXXXXXXXXXXXXXXX"
+  databaseURL: "https://hps-new-bc219-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  apiKey: "AIzaSyBSJk09xKNtGwssaebf3Uw4_PG8_F-dWI4",
+  authDomain: "hps-new-bc219.firebaseapp.com",
+  projectId: "hps-new-bc219",
+  storageBucket: "hps-new-bc219.appspot.com",
+  messagingSenderId: "311656800637",
+  appId: "1:311656800637:web:b651c4d82f0a4d3e5d2c14",
+  measurementId: "G-B35G1KM3YH"
 };
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
